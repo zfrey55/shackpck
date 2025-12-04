f2962b5f51fbcf666b84df4bb95c833d892adc58
Zach Frey <zfrey07@gmail.com>
2025-12-04 14:17:17 -0500

    Clean up unused files and folders

--- coins/app/checklist-sheets-backup/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { SeriesTabs } from "./SeriesTabs";
-export { CaseTypeSelector } from "./CaseTypeSelector";
-export { ChecklistHeader } from "./ChecklistHeader";
-export { CoinList } from "./CoinList";
-export { LoadingState } from "./LoadingState";
-export { ErrorState } from "./ErrorState";
-export { WarningBanner } from "./WarningBanner";
-
